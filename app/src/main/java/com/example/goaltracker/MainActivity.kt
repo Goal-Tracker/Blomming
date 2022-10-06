a50d2bfeabe4b36b28fd6b92fda0abe3a26adc80
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-10-07 06:08:33 +0900

    Update MainActivity Show Ongoing Goal List

--- app/src/main/java/com/example/goaltracker/MainActivity.kt
+++ app/src/main/java/com/example/goaltracker/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.goaltracker
 
+import android.content.ContentValues
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import android.view.MenuItem
 import android.widget.ImageButton
@@ -14,10 +16,20 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.drawer_main.*
 import kotlinx.android.synthetic.main.main_toolbar.*
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    val db = FirebaseFirestore.getInstance()
+
+    lateinit var goalRecordOngoingAdapter: GoalRecordAdapter
+    val onGoingGoalDatas = ArrayList<GoalRecordData>()
+
+    private lateinit var rv_goal : RecyclerView
 
     //어댑터 연결
     //private lateinit var adapter: MainAdapter
@@ -49,6 +61,66 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             drawer_layout.openDrawer(GravityCompat.END)
         }
 
+
+        rv_goal = findViewById(R.id.rv_goal)
+
+        goalRecordOngoingAdapter = GoalRecordAdapter(this)
+        rv_goal.adapter = goalRecordOngoingAdapter
+
+        // 추후엔 Dataframe에서 가져다 사용하기
+        val temp_goal_lsit = arrayOf("a6jyD0k2MSJliDJq1wHb", "IyJXNQPcIx2a5EzLUoeN")
+
+        temp_goal_lsit.forEach { goal_id ->
+            Log.d(ContentValues.TAG, "goal id : $goal_id")
+            val goal_db = db.collection("Goal").document(goal_id)
+
+            goal_db.addSnapshotListener { snapshot, e ->
+                var teamNameList = arrayListOf<String>()
+                var teamThemeList = arrayListOf<String>()
+                val goal_day = snapshot?.get("day").toString().toInt()
+                val start_day = snapshot?.get("startDay").toString()
+                val start_day_str = start_day.replace("-", ".")
+                val end_day = snapshot?.get("endDay").toString()
+                val end_day_str = end_day.replace("-", ".")
+                val past_date = pastCalc(start_day);
+
+                val title = snapshot?.get("title") as String
+
+                if (past_date <= goal_day) {
+                    // 데이터 한 번만 가져오기
+                    db.collection("Goal").document(goal_id).collection("team")
+                        .whereEqualTo("request", true)
+                        .get()
+                        .addOnSuccessListener { result ->
+                            for (document in result) {
+                                teamNameList.add(document["userName"].toString())
+                                teamThemeList.add(document["profileColor"].toString())
+                            }
+
+                            onGoingGoalDatas.add(
+                                GoalRecordData(
+                                    goalId = goal_id,
+                                    title = title,
+                                    participateNum = result.size(),
+                                    startDate = start_day_str,
+                                    endDate = end_day_str,
+                                    todayNum = past_date,
+                                    stampNum = goal_day,
+                                    teamNameList = teamNameList,
+                                    teamThemeList = teamThemeList
+                                )
+                            )
+
+                            goalRecordOngoingAdapter.goalDatas = onGoingGoalDatas
+                            goalRecordOngoingAdapter.notifyDataSetChanged()
+                        }
+                        .addOnFailureListener { exception ->
+                            Log.d(ContentValues.TAG, "Error getting documents: ", exception)
+                        }
+                }
+            }
+        }
+
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
@@ -71,13 +143,28 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     override fun onBackPressed() {
-        if(drawer_layout.isDrawerOpen(GravityCompat.END)){
+        if (drawer_layout.isDrawerOpen(GravityCompat.END)) {
             drawer_layout.closeDrawers()
             Toast.makeText(this, "back btn clicked", Toast.LENGTH_SHORT).show()
-        } else{
+        } else {
             super.onBackPressed()
         }
     }
+
+    private fun pastCalc(first_day: String): Int {
+        var today = Calendar.getInstance()
+
+        var first_date = first_day + " 00:00:00"
+        var sf = SimpleDateFormat("yyyy-MM-dd 00:00:00")
+        var date = sf.parse(first_date)
+
+        var calcDate = (today.time.time - date.time) / (60 * 60 * 24 * 1000)
+
+        Log.d("test: 날짜!!", "$calcDate 일 차이남!!")
+
+        return (calcDate+1).toInt()
+    }
+
 
 //    fun observerData() {
 //        viewModel.fetchData().observe(this, Observer {
