265fcaccb6831304c82c3b191b6ef1e7b95ebfaa
jhsseonn <hosu0125@gmail.com>
2022-10-07 15:01:55 +0900

    Notification 완료

--- app/src/main/java/com/example/goaltracker/NoticeAdapter.kt
+++ app/src/main/java/com/example/goaltracker/NoticeAdapter.kt
@@ -1,21 +1,21 @@
 package com.example.goaltracker
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.ListAdapter
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class NoticeAdapter (private val context:Context) : RecyclerView.Adapter<NoticeAdapter.ViewHolder>(){
 
-    private var notices = mutableListOf<Notification>()
-
-    fun setListData(data:MutableList<Notification>){
-        notices = data
-    }
+    private var noticeDto = mutableListOf<Notifications>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoticeAdapter.ViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.notice_layer, parent, false)
@@ -24,18 +24,20 @@ class NoticeAdapter (private val context:Context) : RecyclerView.Adapter<NoticeA
     }
 
     override fun onBindViewHolder(holder: NoticeAdapter.ViewHolder, position: Int) {
-
+        holder.bind(noticeDto[position])
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = noticeDto.size
 
     inner class ViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){
-        val name : TextView = itemView.findViewById(R.id.notice_profile_name)
-        val content : TextView = itemView.findViewById(R.id.notice_text)
+        val noticeName : TextView = itemView.findViewById(R.id.notice_profile_name)
+        val noticeContent : TextView = itemView.findViewById(R.id.notice_text)
+        val acceptButton : Button = itemView.findViewById(R.id.notice_button)
 
+        fun bind(item: Notifications) {
+            noticeName.text = item.userName
+            noticeContent.text = item.message
+        }
     }
-
 
 }
