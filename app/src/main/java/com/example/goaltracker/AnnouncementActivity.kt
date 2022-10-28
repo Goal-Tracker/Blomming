0958de4305c1c107c9a04b3ef64092f37d2297f9
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-10-28 14:19:46 +0900

    Add announcement page

---
+++ app/src/main/java/com/example/goaltracker/AnnouncementActivity.kt
@@ -0,0 +1,55 @@
+package com.example.goaltracker
+
+import android.content.ContentValues.TAG
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
+
+class AnnouncementActivity : AppCompatActivity() {
+
+    val db = FirebaseFirestore.getInstance()
+
+    lateinit var announcementAdapter: AnnouncementAdapter
+    lateinit var announcement_list: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_announcement)
+
+        announcement_list = findViewById(R.id.announcement_list)
+
+        announcementAdapter = AnnouncementAdapter(this)
+        announcement_list.adapter = announcementAdapter
+
+        db.collection("Announcement")
+            .get()
+            .addOnSuccessListener { annoucements ->
+                val announcementData = ArrayList<AnnouncementData>()
+                for (announcement in annoucements) {
+                    Log.d(TAG, "${announcement.id} => ${announcement.data}")
+
+                    val subject = announcement.data["subject"].toString()
+                    val content = announcement.data["content"].toString()
+                    val createDate = announcement.data["createDate"].toString()
+
+                    announcementData.add(
+                        AnnouncementData(
+                            subject = subject,
+                            content = content,
+                            createDate = createDate
+                        )
+                    )
+                }
+
+                announcementData.sortBy { it.createDate }
+                announcementAdapter.announcementDatas = announcementData
+                announcementAdapter.notifyDataSetChanged()
+            }
+            .addOnFailureListener { exception ->
+                Log.d(TAG, "Error getting documents: ", exception)
+            }
+
+    }
+}
