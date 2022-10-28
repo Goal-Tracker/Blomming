1299ab86ec63bccbc9efd5c27a7080d2d26ca4ac
youjin09222 <68536115+youjin09222@users.noreply.github.com>
2022-10-28 13:07:33 +0900

    [Add Goal] 골 추가 수정

--- app/src/main/java/com/example/goaltracker/AddGoal.kt
+++ app/src/main/java/com/example/goaltracker/AddGoal.kt
@@ -17,15 +17,21 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.goaltracker.databinding.ItemMemberBinding
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.auth.User
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_add_goal.*
+import kotlinx.android.synthetic.main.activity_add_goal.recyclerview
+import kotlinx.android.synthetic.main.activity_add_goal.searchWord
+import kotlinx.android.synthetic.main.activity_edit_goal.*
 import kotlinx.android.synthetic.main.item_member.view.*
 import nl.bryanderidder.themedtogglebuttongroup.ThemedButton
 import nl.bryanderidder.themedtogglebuttongroup.ThemedToggleButtonGroup
+import java.lang.reflect.Array.set
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.TimeUnit
@@ -34,27 +40,25 @@ import java.util.concurrent.TimeUnit
 class AddGoal : AppCompatActivity() {
 
     var firestore : FirebaseFirestore? = null
-    val db = FirebaseFirestore.getInstance()    // Firestore 인스턴스 선언
-    private val currentUser = Firebase.auth.currentUser?.uid // 현재
 
-    lateinit var goalName: EditText         // 이름
-    lateinit var first_day: EditText        // 시작일
-    lateinit var last_day: EditText         // 종료일
-    lateinit var Memo: EditText             // 메모
+    lateinit var title: EditText         // 이름
+    lateinit var startDay: EditText        // 시작일
+    lateinit var endDay: EditText         // 종료일
+    lateinit var memo: EditText             // 메모
     lateinit var save_btn: Button           // 저장
     lateinit var close_btn: ImageButton     // 메인 화면으로
     lateinit var searchBtn: ImageButton     // 검색
 
     // 달력
-    var first_calendar = Calendar.getInstance()
-    var last_calendar = Calendar.getInstance()
+    var startDay_calendar = Calendar.getInstance()
+    var endDay_calendar = Calendar.getInstance()
 
 
     lateinit var document : String
 
     // ArrayList 생성
     var FriendsList : ArrayList<Friends> = arrayListOf()
-    val teamList = ArrayList<String>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -75,11 +79,11 @@ class AddGoal : AppCompatActivity() {
         firestore = FirebaseFirestore.getInstance()
         searchBtn = findViewById(R.id.searchBtn)
 
-        goalName = findViewById(R.id.goalName)
-        first_day = findViewById(R.id.first_day)
-        last_day = findViewById(R.id.last_day)
+        title = findViewById(R.id.title)
+        startDay = findViewById(R.id.startDay)
+        endDay = findViewById(R.id.endDay)
         save_btn = findViewById(R.id.save_btn)
-        Memo = findViewById(R.id.Memo)
+        memo = findViewById(R.id.memo)
         close_btn = findViewById(R.id.close_btn)
 
         recyclerview.adapter = RecyclerViewAdapter()
@@ -103,47 +107,44 @@ class AddGoal : AppCompatActivity() {
         // 시작일 : 현재 날짜
         val myFormat = "yyyy-MM-dd" // mention the format you need
         val sdf = SimpleDateFormat(myFormat, Locale.US)
-        first_day.setText(sdf.format(first_calendar.time))
+        startDay.setText(sdf.format(startDay_calendar.time))
 
         // 종료일
         themedButtonGroup.setOnSelectListener { button : ThemedButton ->
             if (btn_7.isSelected){
                 // 종료일 : 7일 뒤
-                last_calendar.add(Calendar.DATE, +7)
+                endDay_calendar.add(Calendar.DATE, +7)
             }
             if (btn_30.isSelected){
                 // 종료일 : 30일 뒤
-                last_calendar.add(Calendar.DATE, +30)
+                endDay_calendar.add(Calendar.DATE, +30)
             }
             if (btn_50.isSelected){
                 // 종료일 : 50일 뒤
-                last_calendar.add(Calendar.DATE, +50)
+                endDay_calendar.add(Calendar.DATE, +50)
             }
             if (btn_100.isSelected){
                 // 종료일 : 100일 뒤
-                last_calendar.add(Calendar.DATE, +100)
+                endDay_calendar.add(Calendar.DATE, +100)
             }
-            last_day.setText(sdf.format(last_calendar.time))
+            endDay.setText(sdf.format(endDay_calendar.time))
         }
 
         // 데이터 저장
         save_btn.setOnClickListener {
 
             val goal = hashMapOf(
-                "GoalName" to goalName.text.toString(),
-                "StartDay" to first_day.text.toString(),
-                "EndDay" to last_day.text.toString(),
-                "Memo" to Memo.text.toString(),
-                "Action" to true,
-                "Day" to fewDay(),  //날짜 차이 계산
-                "Team" to teamList // 팀원 uid (배열)
+                "title" to title.text.toString(),
+                "startDay" to startDay.text.toString(),
+                "endDay" to endDay.text.toString(),
+                "memo" to memo.text.toString(),
+                "action" to true,
+                "stampId" to UUID.randomUUID().toString(),
+                "day" to fewDay(),  //날짜 차이 계산
+                // "Team" to teamList // 팀원 uid (배열)
             )
+            firestore!!.collection("Goal").document(title.text.toString()).set(goal)
 
-            firestore!!.collection("Goal")
-                .add(goal)
-                .addOnSuccessListener { documentReference -> Log.d("DatabaseTest", documentReference.id)
-                    document =  documentReference.id}
-                .addOnFailureListener { exception -> Log.d("DatabaseTest", exception.message!!) }
 
             Toast.makeText(this, "저장되었습니다.", Toast.LENGTH_SHORT).show()
         }
@@ -152,30 +153,30 @@ class AddGoal : AppCompatActivity() {
     fun showDatePicker() {
 
         // DatePicker
-        first_day.setText(SimpleDateFormat("yyyy-MM-dd").format(System.currentTimeMillis()))
-        last_day.setText(SimpleDateFormat("yyyy-MM-dd").format(System.currentTimeMillis()))
+        startDay.setText(SimpleDateFormat("yyyy-MM-dd").format(System.currentTimeMillis()))
+        endDay.setText(SimpleDateFormat("yyyy-MM-dd").format(System.currentTimeMillis()))
 
 
         // 시작일 직접 설정
         val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
-            first_calendar.set(Calendar.YEAR, year)
-            first_calendar.set(Calendar.MONTH, monthOfYear)
-            first_calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            startDay_calendar.set(Calendar.YEAR, year)
+            startDay_calendar.set(Calendar.MONTH, monthOfYear)
+            startDay_calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
 
             val myFormat = "yyyy-MM-dd" // mention the format you need
             val sdf = SimpleDateFormat(myFormat, Locale.US)
-            first_day.setText(sdf.format(first_calendar.time))
+            startDay.setText(sdf.format(startDay_calendar.time))
         }
 
         // 시작일 클릭 시 달력 팝업
-        first_day.setOnClickListener {
+        startDay.setOnClickListener {
 
             Log.d("Clicked", "Interview Date Clicked")
 
             val dialog = DatePickerDialog(this, dateSetListener,
-                first_calendar.get(Calendar.YEAR),
-                first_calendar.get(Calendar.MONTH),
-                first_calendar.get(Calendar.DAY_OF_MONTH))
+                startDay_calendar.get(Calendar.YEAR),
+                startDay_calendar.get(Calendar.MONTH),
+                startDay_calendar.get(Calendar.DAY_OF_MONTH))
 
             // 시작일은 오늘 이전부터 선택
             dialog.datePicker.maxDate = CalendarHelper.getCurrentDateInMills()
@@ -185,25 +186,25 @@ class AddGoal : AppCompatActivity() {
 
         // 종료일 직접 설정
         val dateSetListener2 = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
-            last_calendar.set(Calendar.YEAR, year)
-            last_calendar.set(Calendar.MONTH, monthOfYear)
-            last_calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            endDay_calendar.set(Calendar.YEAR, year)
+            endDay_calendar.set(Calendar.MONTH, monthOfYear)
+            endDay_calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
 
             val myFormat = "yyyy-MM-dd" // mention the format you need
             val sdf = SimpleDateFormat(myFormat, Locale.US)
 
-            last_day.setText(sdf.format(last_calendar.time))
+            endDay.setText(sdf.format(endDay_calendar.time))
         }
 
         // 종료일 클릭 시 달력 팝업
-        last_day.setOnClickListener {
+        endDay.setOnClickListener {
 
             Log.d("Clicked", "Interview Date Clicked")
 
             val dialog = DatePickerDialog(this, dateSetListener2,
-                last_calendar.get(Calendar.YEAR),
-                last_calendar.get(Calendar.MONTH),
-                last_calendar.get(Calendar.DAY_OF_MONTH))
+                endDay_calendar.get(Calendar.YEAR),
+                endDay_calendar.get(Calendar.MONTH),
+                endDay_calendar.get(Calendar.DAY_OF_MONTH))
 
             // 종료일은 오늘 이후부터 선택
             dialog.datePicker.minDate = CalendarHelper.getCurrentDateInMills()
@@ -214,7 +215,7 @@ class AddGoal : AppCompatActivity() {
     // 두 날짜 차이 계산
     fun fewDay(): Long {
         //milliseconds -> day로 변환
-        return TimeUnit.MILLISECONDS.toDays(last_calendar.timeInMillis - first_calendar.timeInMillis)
+        return TimeUnit.MILLISECONDS.toDays(endDay_calendar.timeInMillis - startDay_calendar.timeInMillis)
     }
 
     inner class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -251,39 +252,31 @@ class AddGoal : AppCompatActivity() {
             fun addFriendsBtnOnclick(item: Friends){
 
                 binding.checkBox.setOnClickListener {
+                    val request : Boolean
+
                     // 체크한 친구만 Goal에 추가
-                    when ((it as CheckBox).isChecked) {
-                        true -> teamList.add(item.Uid.toString())
-                        false -> teamList.remove(item.Uid.toString()) // 리스트에서 제거
+                    if ((it as CheckBox).isChecked) {
+                        request = false
+
+                        val team = hashMapOf(
+                            "userName" to item.UserName.toString(),
+                            "uid" to item.Uid.toString(),
+                            "request" to request,
+                            "profileColor" to item.UserColor.toString()
+                        )
+
+                        firestore!!.collection("Goal")
+                            .document(title.text.toString())
+                            .collection("team")
+                            .document(item.Uid.toString()).set(team)
                     }
-
-                    if(item.Uid != currentUser){
-                        val friendRef = firestore?.collection("Goal")?.document(item.Uid.toString())
-                        val myRef = firestore?.collection("Goal")?.document("$currentUser")
-
-                        friendRef!!.get()
-                            .addOnSuccessListener { friendDocument ->
-                                myRef!!.get()
-                                    .addOnSuccessListener { myDocument->
-                                        val friend: MutableMap<String, String> = HashMap()
-                                        val mine: MutableMap<String, String> = HashMap()
-
-                                        // 친구 리스트가 없는 경우
-                                        if( myDocument.get("Team") == null ){
-                                            mine[item.Uid.toString()] = "request"
-                                            // 배열 요소 업데이트
-                                            myRef.update("Team", FieldValue.arrayUnion(mine)) // arrayUnion()은 배열에 없는 요소만 추가
-
-                                            friend[currentUser.toString()] = "requested"
-                                            friendRef.update("Team", FieldValue.arrayUnion(friend))
-
-                                            Toast.makeText(this@AddGoal,item.UserName.toString()+"에게 친구요청을 보냈습니다.", Toast.LENGTH_LONG).show()
-                                            Log.d(friendDocument.get("UserName").toString(),"에게 친구요청 성공")
-                                        }
-                                    }
-                                    .addOnFailureListener {
-                                    }
-                            }
+                    // 체크 해제 시 삭제
+                    else {
+                        firestore!!.collection("Goal")
+                            .document(title.text.toString())
+                            .collection("team")
+                            .document(item.Uid.toString())
+                            .delete()
                     }
                 }
             }
@@ -305,7 +298,6 @@ class AddGoal : AppCompatActivity() {
             return FriendsList.size
         }
 
-
         // 파이어스토어에서 데이터를 불러와서 검색어가 있는지 판단
         fun search(searchWord : String) {
             firestore?.collection("Account")?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
@@ -318,7 +310,6 @@ class AddGoal : AppCompatActivity() {
                         FriendsList.add(item!!)
                     }
                 }
-
                 notifyDataSetChanged()
             }
         }
