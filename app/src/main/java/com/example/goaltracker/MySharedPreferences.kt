eb941b56a11b8f879386447a232ac3b7588dfe3a
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-11-04 16:24:08 +0900

    Update theme setter & getter

--- app/src/main/java/com/example/goaltracker/MySharedPreferences.kt
+++ app/src/main/java/com/example/goaltracker/MySharedPreferences.kt
@@ -2,6 +2,7 @@ package com.example.goaltracker
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.content.res.Resources
 
 object MySharedPreferences {
     private val MY_ACCOUNT : String = "account"
@@ -40,5 +41,33 @@ object MySharedPreferences {
     fun getUserColor(context: Context): String {
         val prefs : SharedPreferences = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
         return prefs.getString("userColor", "").toString()
+    }
+
+    fun setTheme(context: Context, theme: String){
+        val prefs : SharedPreferences = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        val editor : SharedPreferences.Editor = prefs.edit()
+
+        var themeName = R.style.Theme_Coral
+        if (theme == "#f69b94") {
+            themeName = R.style.Theme_Coral
+        } else if (theme == "#96b0e5") {
+            themeName = R.style.Theme_Blue
+        } else if (theme == "#f8c8c4") {
+            themeName = R.style.Theme_BabyPink
+        } else if (theme == "#fcdcce") {
+            themeName = R.style.Theme_LightOrange
+        } else if (theme == "#92b9e2") {
+            themeName = R.style.Theme_LightBlue
+        } else if (theme == "#aad3d7") {
+            themeName = R.style.Theme_Mint
+        }
+
+        editor.putInt("theme", themeName)
+        editor.commit()
+    }
+
+    fun getTheme(context: Context): Int {
+        val prefs : SharedPreferences = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        return prefs.getInt("theme", R.style.Theme_Mint)
     }
 }
