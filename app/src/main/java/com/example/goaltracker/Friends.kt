135227cd627d6d2eae96267e80674b2a44547f37
minji05 <smj95133@likelion.org>
2022-12-16 14:03:21 +0900

    [FIX] minji- 상태메시지 추가 및 신고기능 완료

--- app/src/main/java/com/example/goaltracker/Friends.kt
+++
@@ -1,7 +0,0 @@
-package com.example.goaltracker
-
-class Friends (var userName : String? = null,
-              var email : String? = null,
-              var uid : String? = null,
-              var status : String? = null,
-              var userColor : String? = null)
